b625597822281221c6159bf6447516f3cbe82757
Vince Gellár <gellar.vince@gmail.com>
2018-08-02 21:53:00 +0200

    Update example description

--- examples/WebRadioDemo/WebRadioDemo.ino
+++ examples/WebRadioDemo/WebRadioDemo.ino
@@ -1,4 +1,9 @@
 /**
+  A simple stream handler to play web radio stations using ESP8266
+
+  Copyright (C) 2018 Vince Gellár (github.com/vincegellar)
+  Licensed under GNU GPL v3
+  
   Wiring:
   --------------------------------
   | VS1053  | ESP8266 |  Other   |
@@ -18,8 +23,21 @@
   -VS1053 library by baldram (https://github.com/baldram/ESP_VS1053_Library)
   -ESP8266Wifi
 
+  To run this example define the platformio.ini as below.
+
+  [env:nodemcuv2]
+  platform = espressif8266
+  board = nodemcuv2
+  framework = arduino
+  build_flags = -D PIO_FRAMEWORK_ARDUINO_LWIP2_HIGHER_BANDWIDTH
+
+  lib_deps =
+    ESP_VS1053_Library
+
   Instructions:
   -Build the hardware
+    (please find an additional description and Fritzing's schematic here:
+     https://github.com/vincegellar/Simple-Radio-Node#wiring)
   -Set the station in this file
   -Upload the program
 

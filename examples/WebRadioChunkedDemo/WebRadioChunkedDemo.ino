035ddef9c7180c34af747a47a352d3c8899afbac
Witold Markowski <witold.a.markowski@gmail.com>
2020-03-18 22:18:22 +0100

    Fix the glitches.

--- examples/WebRadioChunkedDemo/WebRadioChunkedDemo.ino
+++ examples/WebRadioChunkedDemo/WebRadioChunkedDemo.ino
@@ -56,6 +56,63 @@ void playRing(uint8_t b) {
 	}
 }
 
+uint8_t middleBuffer[8];
+uint8_t middleBufferCount = 0;
+
+void fix_middle_ring() {
+	if (middleBuffer[0] != '\r') {
+		return;
+	}
+	if (middleBuffer[1] != '\n') {
+		return;
+	}
+
+	if (middleBuffer[4] == '\r' && middleBuffer[5] == '\n') {
+		// \r\n<byte><byte>\r\n
+		middleBufferCount = 2;
+		Serial.println("Flush middle ring");
+
+		return;
+	}
+
+	if (middleBuffer[5] == '\r' && middleBuffer[6] == '\n') {
+		// \r\n<byte><byte><byte>\r\n
+		middleBufferCount = 1;
+		Serial.println("Flush middle ring");
+
+		return;
+	}
+
+	if (middleBuffer[6] == '\r' && middleBuffer[7] == '\n') {
+		// \r\n<byte><byte><byte><byte>\r\n
+		middleBufferCount = 0;
+		Serial.println("Flush middle ring");
+
+		return;
+	}
+}
+
+void put_middle_ring(uint8_t newValue) {
+	if (middleBufferCount == 8) {
+		putring(middleBuffer[0]);
+	}
+
+	middleBuffer[0] = middleBuffer[1];
+	middleBuffer[1] = middleBuffer[2];
+	middleBuffer[2] = middleBuffer[3];
+	middleBuffer[3] = middleBuffer[4];
+	middleBuffer[4] = middleBuffer[5];
+	middleBuffer[5] = middleBuffer[6];
+	middleBuffer[6] = middleBuffer[7];
+	middleBuffer[7] = newValue;
+	middleBufferCount++;
+	if (middleBufferCount > 8) {
+		middleBufferCount = 8;
+	}
+
+	fix_middle_ring();
+}
+
 void setup() {
 	Serial.begin(115200);
 	mp3buff = (uint8_t*) malloc(VS_BUFF_SIZE);
@@ -100,7 +157,7 @@ void loop() {
 			maxfilechunk = CLIENT_BUFF;
 		}
 		while (ringspace() && maxfilechunk--) {
-			putring(client.read());
+			put_middle_ring(client.read());
 		}
 		yield();
 		while (rcount && (player.data_request())) {
